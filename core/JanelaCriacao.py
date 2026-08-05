4ff30b0f736443f37ffbd383179af710151f0e7a
DarkVinikicius13 <viniciusteixeira4012@gmail.com>
2026-08-05 19:07:04 -0300

    Added image selection

--- core/JanelaCriacao.py
+++ core/JanelaCriacao.py
@@ -1,5 +1,5 @@
-from PyQt5.QtWidgets import QWidget, QLabel, QWidget, QVBoxLayout, QHBoxLayout, QLineEdit
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QWidget, QLabel, QWidget, QVBoxLayout, QHBoxLayout, QLineEdit, QFileDialog, QPushButton
+from PyQt5.QtGui import QIcon, QPixmap
 
 class Janela_Criacao(QWidget): #monta a janela
     def __init__(self):
@@ -8,12 +8,28 @@ class Janela_Criacao(QWidget): #monta a janela
         self.setGeometry(500, 500, 350, 150)
         self.setWindowIcon(QIcon("images/icon.png"))
 
+        #Inicio da definição das caixas de texto para criação dos personagens.
         self.Nome = QLabel("Nome do Personagem: ", self)
         self.Nome_Input = QLineEdit(self)
         
+        self.Idade = QLabel("Idade: ", self)
+        self.Idade_Input = QLineEdit(self)
+        
+        self.Peso = QLabel("Peso do Personagem: ", self)
+        self.Peso_Input = QLineEdit(self)
+        
+        self.Genero = QLabel("Gênero do Personagem: ", self)
+        self.Genero_Input = QLineEdit(self)
+        
+        self.ImagemEtiqueta = QLabel("Nenhuma imagem selecionada")
+        self.BotaoEscolherImagem = QPushButton("Escolher imagem")
+        self.BotaoEscolherImagem.clicked.connect(self.Selecionar_Imagem)
+
+        #Fim
+        
         self.Interface_Criacao()
 
-    def Interface_Criacao(self):
+    def Interface_Criacao(self): #Aqui é só a interface, parte estética.
         
         hbox = QHBoxLayout()
         vbox = QVBoxLayout()
@@ -21,5 +37,35 @@ class Janela_Criacao(QWidget): #monta a janela
         hbox.addWidget(self.Nome)
         hbox.addWidget(self.Nome_Input)
 
+        hbox.addWidget(self.Idade)
+        hbox.addWidget(self.Idade_Input)
+        
+        hbox.addWidget(self.Peso)
+        hbox.addWidget(self.Peso_Input)
+        
+        hbox.addWidget(self.Genero)
+        hbox.addWidget(self.Genero_Input)
+
+        hbox.addWidget(self.ImagemEtiqueta)
+        hbox.addWidget(self.BotaoEscolherImagem)
+
         vbox.addLayout(hbox)
         self.setLayout(vbox)
+
+    
+    def Selecionar_Imagem(self):
+        caminho, _ = QFileDialog.getOpenFileName(
+            self,
+            "Escolha uma imagem",
+            "",
+            "Imagens (*.png *.jpg *.jpeg *.bmp *.webp)"
+        )
+
+        if caminho:
+            print(caminho)
+
+            pixmap = QPixmap(caminho)
+            pixmap = pixmap.scaled(
+                self.ImagemEtiqueta.width(),
+                self.ImagemEtiqueta.height()
+            )
